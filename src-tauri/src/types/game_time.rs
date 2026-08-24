1a37b068c9bd071ac1f0975094c76fe47d1c5b89
Luca Joss <43531661+ljoss17@users.noreply.github.com>
2026-08-24 12:27:41 +0200

    Add tests for GameTime struct and fix bugs

--- src-tauri/src/types/game_time.rs
+++ src-tauri/src/types/game_time.rs
@@ -24,10 +24,10 @@ impl Add for GameTime {
 
     // The addition does not take into account day changes
     fn add(self, rhs: Self) -> Self::Output {
-        let mut hour = (self.hour() + rhs.hour()) % 23;
+        let mut hour = (self.hour() + rhs.hour()) % 24;
         let total_minutes = self.minute() + rhs.minute();
         let minute = if total_minutes > 59 {
-            hour = (hour + 1) % 23;
+            hour = (hour + 1) % 24;
             total_minutes % 60
         } else {
             total_minutes
@@ -51,9 +51,68 @@ impl GameTime {
     pub fn minute(&self) -> u8 {
         self.minute
     }
+}
 
-    pub fn increment(&mut self, amount: GameTime) {
-        self.hour += amount.hour();
-        self.minute += amount.minute();
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_accepts_boundary_values() {
+        assert!(GameTime::new(0, 0).is_ok());
+        assert!(GameTime::new(23, 59).is_ok());
+    }
+
+    #[test]
+    fn new_rejects_invalid_hour() {
+        let result = GameTime::new(24, 0);
+        assert!(matches!(result, Err(AppError::InvalidTime(24, 0))));
+    }
+
+    #[test]
+    fn new_rejects_invalid_minute() {
+        let result = GameTime::new(0, 60);
+        assert!(matches!(result, Err(AppError::InvalidTime(0, 60))));
+    }
+
+    #[test]
+    fn hour_and_minute_return_constructed_values() {
+        let time = GameTime::new(14, 5).unwrap();
+        assert_eq!(time.hour(), 14);
+        assert_eq!(time.minute(), 5);
+    }
+
+    #[test]
+    fn display_formats_with_leading_zeros() {
+        let time = GameTime::new(9, 5).unwrap();
+        assert_eq!(time.to_string(), "09:05");
+    }
+
+    #[test]
+    fn add_sums_hours_and_minutes_without_carry() {
+        let start = GameTime::new(9, 0).unwrap();
+        let duration = GameTime::new(1, 30).unwrap();
+        assert_eq!(start + duration, GameTime::new(10, 30).unwrap());
+    }
+
+    #[test]
+    fn add_carries_minute_overflow_into_hour() {
+        let start = GameTime::new(9, 45).unwrap();
+        let duration = GameTime::new(0, 30).unwrap();
+        assert_eq!(start + duration, GameTime::new(10, 15).unwrap());
+    }
+
+    #[test]
+    fn add_leaves_hour_23_unchanged_when_adding_zero() {
+        let start = GameTime::new(23, 0).unwrap();
+        let duration = GameTime::new(0, 0).unwrap();
+        assert_eq!(start + duration, GameTime::new(23, 0).unwrap());
+    }
+
+    #[test]
+    fn add_wraps_past_midnight_at_hour_24() {
+        let start = GameTime::new(23, 45).unwrap();
+        let duration = GameTime::new(0, 30).unwrap();
+        assert_eq!(start + duration, GameTime::new(0, 15).unwrap());
     }
 }
